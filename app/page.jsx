4d852e12eb73a8e397cdd7e95a60fe6e9aa2ac54
cxddcx271 <21020290@vnu.edu.vn>
2023-11-27 20:31:32 +0700

    create user list + new 'page.jsx' in folder home

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,10 @@
-import Header from './HomePage/header'
-import Hero from './HomePage/hero'
-import Features from './HomePage/features'
-import Work from './HomePage/work'
-import Search from './HomePage/search'
-import Footer from './HomePage/footer'
-import Trial from './HomePage/trial'
+import PageHome from "./home/page"
+// import User from "./user/page"
 
 export default function Home() {
   return (
-    <div class="main">
-      <Header />
-      <Hero />
-      <Search />
-      <Features />
-      <Work />
-      <Trial />
-      <Footer />
-    </div>
+    <>
+      <PageHome />
+    </>
   )
 }
