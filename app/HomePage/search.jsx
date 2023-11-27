5b1a890d71713d2b02272f052cecf2dcbf05d062
batondesu <trantoan29072003@gmail.com>
2023-11-27 21:27:25 +0700

    update

--- app/HomePage/search.jsx
+++ app/HomePage/search.jsx
@@ -2,26 +2,26 @@ import imageAsset11 from './assets/imgs/elements/blob.svg'
 
 export default function Search() {
     return (
-        <section class="py-20 bg-top bg-no-repeat" id="search" style={{ backgroundImage: `url(${imageAsset11.src})` }}>
-            <div class="container px-4 mx-auto">
-                <div class="relative py-20 px-4 lg:p-20">
-                    <div class="max-w-lg mx-auto text-center">
-                        <h2 class="mb-4 text-3xl lg:text-4xl font-bold font-heading wow animate__animated animate__fadeInUp">
+        <section className="py-20 bg-top bg-no-repeat" id="search" style={{ backgroundImage: `url(${imageAsset11.src})` }}>
+            <div className="container px-4 mx-auto">
+                <div className="relative py-20 px-4 lg:p-20">
+                    <div className="max-w-lg mx-auto text-center">
+                        <h2 className="mb-4 text-3xl lg:text-4xl font-bold font-heading wow animate__animated animate__fadeInUp">
                             <span>Tra cứu</span>
-                            <span class="text-blue-500"> BƯU GỬI </span>
+                            <span className="text-blue-500"> BƯU GỬI </span>
                         </h2>
-                        <p class="mb-8 text-blueGray-400 wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
+                        <p className="mb-8 text-blueGray-400 wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
                             Mã bưu gửi (tra nhiều bill thêm dấu phẩy giữa các bill VD: EB125966888VN, EI125556888VN)
                         </p>
-                        <div class="p-4 bg-white rounded-lg flex flex-wrap max-w-md mx-auto wow animate__animated animate__fadeInUp" data-wow-delay=".5s">
-                            <div class="flex w-full md:w-2/3 px-3 mb-3 md:mb-0 md:mr-6 bg-blueGray-100 rounded">
-                                <svg class="h-6 w-6 my-auto text-blueGray-500" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" fill="currentColor">
+                        <div className="p-4 bg-white rounded-lg flex flex-wrap max-w-md mx-auto wow animate__animated animate__fadeInUp" data-wow-delay=".5s">
+                            <div className="flex w-full md:w-2/3 px-3 mb-3 md:mb-0 md:mr-6 bg-blueGray-100 rounded">
+                                <svg className="h-6 w-6 my-auto text-blueGray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                                 </svg>
-                                <input class="w-full pl-3 py-4 text-xs text-blueGray-400 font-semibold leading-none bg-blueGray-100 outline-none" type="text" placeholder="Nhập mã bưu gửi" />
+                                <input className="w-full pl-3 py-4 text-xs text-blueGray-400 font-semibold leading-none bg-blueGray-100 outline-none" type="text" placeholder="Nhập mã bưu gửi" />
                             </div>
-                            <button class="w-full md:w-auto py-4 px-8 text-xs text-white font-semibold leading-none bg-blue-400 hover:bg-blue-500 rounded" type="submit">Đến</button>
+                            <button className="w-full md:w-auto py-4 px-8 text-xs text-white font-semibold leading-none bg-blue-400 hover:bg-blue-500 rounded" type="submit">Đến</button>
                         </div>
                     </div>
                 </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+// import Home from './page'
+import React from 'react'
 import Header from './HomePage/header'
 import Hero from './HomePage/hero'
 import Features from './HomePage/features'
@@ -6,16 +8,40 @@ import Search from './HomePage/search'
 import Footer from './HomePage/footer'
 import Trial from './HomePage/trial'
 
+import Script from 'next/script'
+
+import './HomePage/assets/css/tailwind-built.css'
+
 export default function Home() {
   return (
-    <div class="main">
-      <Header />
-      <Hero />
-      <Search />
-      <Features />
-      <Work />
-      <Trial />
-      <Footer />
-    </div>
+    <body className="bg-white text-body font-body">
+        <Header/>
+        
+        <div className="main">
+          <Hero />
+          <Search />
+          <Features />
+          <Work />
+          <Trial />
+        </div> 
+        <Script data-cfasync="false" src="./HomePage/assets/js/vendor/email-decode.min.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/modernizr-3.6.0.min.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/jquery-3.6.0.min.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/waypoints.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/counterup.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/slick.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/wow.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/scrollup.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/smooth.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/textType.js"></Script>
+        <Script src="./HomePage/assets/js/vendor/mobile-menu.js"></Script>
+        <Script src="./HomePage/assets/js/main.js"></Script>
+        <a id="scrollUp" href="#top" style={{ position: 'fixed', zIndex: '2147483647' }}>
+          <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
+          </svg>
+        </a>
+        <Footer /> 
+    </body>
   )
 }
