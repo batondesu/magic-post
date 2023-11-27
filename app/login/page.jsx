7fe680efb2bbf555118acb77b5a822e8ba1a171f
DuongManhMAC <macosx@DuongManh.local>
2023-11-27 11:45:10 +0700

    fix

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,10 @@
 // pages/login.js
 
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+
+import { TiPhone } from "react-icons/ti";
+import { FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
@@ -10,32 +13,52 @@ const Login = () => {
         <h2 className="text-2xl font-bold mb-4 text-center">Đăng nhập</h2>
         <form className="space-y-4">
           <div className="relative">
-            <label htmlFor="phone" className="absolute top-0 left-2 -mt-2 text-xs text-gray-600">
+            <label
+              htmlFor="phone"
+              className="absolute top-0 left-2 -mt-2 text-xs text-gray-600"
+            >
               Số điện thoại
             </label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              className="w-full mt-3 border-2 border-blue-400 rounded-md p-2 focus:outline-none focus:border-blue-600"
-              placeholder="Nhập số điện thoại của bạn"
-            />
+            <div className="">
+              <TiPhone
+                size={30}
+                className="absolute top-5 left-1 text-gray-600"
+              />
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                className="w-full pl-8 mt-3 border-2 border-blue-400 rounded-md p-2 focus:outline-none focus:border-blue-600"
+                placeholder="Nhập số điện thoại của bạn"
+              />
+            </div>
           </div>
           <div className="relative">
-            <label htmlFor="password" className="absolute top-0 left-2 -mt-2 text-xs text-gray-600">
+            <label
+              htmlFor="password"
+              className="absolute top-0 left-2 -mt-2 text-xs text-gray-600"
+            >
               Mật khẩu
             </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full mt-3 border-2 border-blue-400 rounded-md p-2 focus:outline-none focus:border-blue-600"
-              placeholder="Nhập mật khẩu của bạn"
-            />
+            <div>
+              <FaLock
+                size={25}
+                className="absolute top-5 left-1 text-gray-600"
+              />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full pl-8 mt-3 border-2 border-blue-400 rounded-md p-2 focus:outline-none focus:border-blue-600"
+                placeholder="Nhập mật khẩu của bạn"
+              />
+            </div>
           </div>
           <div className="flex justify-between items-center">
             <Link legacyBehavior href="/forgot-password">
-              <a className="text-blue-500 text-xs hover:underline">Quên mật khẩu?</a>
+              <a className="text-blue-500 text-xs hover:underline">
+                Quên mật khẩu?
+              </a>
             </Link>
             <Link legacyBehavior href="/register">
               <a className="text-blue-500 text-xs hover:underline">Đăng ký</a>
